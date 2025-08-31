9f77ae23e194a9dd7c9e4593f8b00adc05814615
Sebastian Waisbrot <seppo0010@gmail.com>
2025-08-31 08:28:02 -0300

    Config in dotenv

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
 
 /**
  * Read environment variables from file.
